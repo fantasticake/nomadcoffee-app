6b1606d7ff67f475b19d9f5545164b6f83af269f
fantasticake <fantasticake21@gmail.com>
2022-10-03 13:05:45 +0900

    preload on splash-screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import { useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import * as SplashScreen from "expo-splash-screen";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(() => {
+    const preload = async () => {
+      await Font.loadAsync(Ionicons.font);
+      await Asset.loadAsync([require("./assets/favicon.png")]);
+    };
+    try {
+      preload();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      SplashScreen.hideAsync();
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
